32de3682d0330dc1e18f023726d1fd21bce2c553
Chris Carpenter <chriscrpntr@gmail.com>
2025-01-18 09:04:32 -0500

    Added Unpivot Command

--- main.ts
+++ main.ts
@@ -5,7 +5,6 @@ function AddForm(){
     SpreadsheetApp.getUi().showSidebar(form);
 }
 
-
 function onOpen(){
     let menu = SpreadsheetApp.getUi().createAddonMenu();
     menu.addItem('Sheets Discord Tools', 'AddForm');
@@ -39,7 +38,6 @@ function cleanRange() {
     var sheet = ss.getActiveSheet();
     var range = sheet.getActiveRange();
     var values = range.getValues();
-
     for (var i = 0; i < values.length; i++) {
         for (var j = 0; j < values[i].length; j++) {
             var cellValue = values[i][j];
@@ -54,4 +52,33 @@ function cleanRange() {
 function cleanAndTrim(text) {
     var cleanedText = text.replace(/[\x00-\x1F\x7F-\x9F]/g, "");
     return cleanedText.trim();
+}
+
+
+function unpivot(input) {
+    const ss = SpreadsheetApp.getActiveSpreadsheet();
+    const sheet = ss.getActiveSheet();
+    const range = sheet.getActiveRange();
+
+    const values = range.getValues();
+    if (!values || values.length < 2 || values[0].length < 2) {
+        SpreadsheetApp.getUi().alert("Selected range must have at least 2 rows and 2 columns.");
+        return;
+    }
+
+    const headers = values[0];
+    const data = values.slice(1);
+    const unpivotedData = [];
+
+    for (let i = 0; i < data.length; i++) {
+        const row = data[i];
+        const idValues = row.slice(0, input);
+
+        for (let j = input; j < headers.length; j++) {
+            unpivotedData.push([...idValues, headers[j], row[j]]);
+        }
+    }
+
+    const newSheet = ss.insertSheet("Unpivoted Data");
+    newSheet.getRange(1, 1, unpivotedData.length, unpivotedData[0].length).setValues(unpivotedData);
 }
